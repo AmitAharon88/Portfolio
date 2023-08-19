06b1d48460bacb3730934effb9a81a7935b91116
AmitAhron88 <aaharon88@gmail.com>
2023-08-19 20:05:14 +0300

    changed to project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
-import SkiAlertPic from '../images/SkiAlert.png'
-import gitHub from '../images/github.png'
-import web from '../images/web.png'
+import SkiAlert from '../images/SkiAlert.png';
+import ProFinder from '../images/ProFinder.png';
+import gitHub from '../images/github.png';
+import web from '../images/web.png';
 import './Projects.css';
 
 const Projects = (props) => {
@@ -8,7 +9,7 @@ const Projects = (props) => {
        <div id='projects' className='projectsContainer'>
           <h1 id='projectsHeader'>Projects</h1>
           <div id='projectContainer'>
-            <img className='projectImg' src={SkiAlertPic} alt='SkiAlert project'/>
+            <img className='projectImg' src={SkiAlert} alt='SkiAlert project'/>
             <div className='projExplanationContainer'>
                <h3 className='projectName'>SkiAlert</h3>
                <p className='projectText'>SkiAlert is a user-friendly platform designed for ski and snowboard enthusiasts, providing a quick and effortless way to access up-to-date snow conditions.</p>
@@ -29,10 +30,10 @@ const Projects = (props) => {
             </div>
           </div>
           <div id='projectContainer2'>
-            <img className='projectImg' src={SkiAlertPic} alt='SkiAlert project'/>
+            <img className='projectImg' src={ProFinder} alt='SkiAlert project'/>
             <div className='projExplanationContainer'>
                <h3 className='projectName'>ProFinder</h3>
-               <p className='projectText'>Profinder is an engaging platform that facilitates seamless connections between students and tutors. By signing in as a student, you gain access to search for tutors based on categories or locations. On the other hand, tutors can register themselves on the platform for students to discover. Profinder is its integrated live chat, which allows students to reach out to tutors directly.</p>
+               <p className='projectText'>Profinder is an engaging platform that facilitates seamless connections between students and tutors. By signing in as a student, you gain access to search for tutors based on categories or locations. On the other hand, tutors can register themselves on the platform for students to discover. Profinder has integreated messenger capability, which allows students to reach out to tutors directly.</p>
                <div className='toolsContainer'>
                   <p className='projectText'>Tools used:</p>
                   <p className='tool'>React</p>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,7 @@
 import linkedin from '../images/linkedin.png'
 import gitHub2 from '../images/github2.png'
 import download from '../images/download.png'
-import cv from '../images/cv.pdf'
+import AmitAharonCV from '../images/AmitAharonCV.pdf'
 import './SideBar.css';
 
 const SideBar = () => {
@@ -13,7 +13,7 @@ const SideBar = () => {
         <a id='sideGitHub' href='https://github.com/AmitAharon88/Hackathon_1'>
             <img className='gitHub2' src={gitHub2} alt='gitHub icon'/>
         </a>
-        <a id='sideDownload'href={cv} download>
+        <a id='sideDownload'href={AmitAharonCV} download>
             <img className='download' src={download} alt='download icon'/>
         </a>
       </div>
